50ebeb30dd0b764443ed0e3ff416ed5c9be18b6b
ktdodd511 <katiedodd@Katies-MacBook-Air-2.local>
2017-01-24 12:08:38 -0800

    new book functionality added

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,26 @@
 class BooksController < ApplicationController
 
   def index
-    @books = Book.all
+    @books = Book.all.order("created_at DESC")
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book Saved!"
+      redirect_to books_path
+    else
+      flash[:notice] = "There was an error."
+      render 'new'
+    end
   end
 
   def show
-    @book = Book.find(params[:id])
+    @book = Book.find(id)
   end
 
 
@@ -14,6 +29,10 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :author, :genre, :date_published, :description)
+  end
+
+  def id
+    params[:id]
   end
 
 
